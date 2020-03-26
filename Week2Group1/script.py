fc1192da3a406f2d0ca7c72876e89ac4c05932a2
Keith C-R <keith@kmddigital.com>
2020-03-26 18:00:14 -0400

    Updated script to check HWE of each site

--- Week2Group1/script.py
+++ Week2Group1/script.py
@@ -8,50 +8,64 @@ def main():
         i = i+1;
         if i > 3:
             lines.append(line);
-    currPair = 0;
+    currSite = 0;
     chromLength = len(lines[0]);
-    totalHetero = 0;
-    totalHomo1 = 0;
-    totalHomo0 = 0;
-    while (currPair < len(lines)):
-        first = lines[currPair];
-        second = lines[currPair+1];
-        currPair = currPair + 2;
-        currSite = 0;
-        while (currSite < chromLength):
-            firstSite = first[currSite:currSite+1];
-            secondSite = second[currSite:currSite+1];
+    totalAlleles = len(lines)
+    isNot = []
+    isEq = []
+    boundary = 0.06
+    while (currSite < chromLength):
+        currPair = 0;
+        totalHetero = 0
+        totalHomo0 = 0
+        totalHomo1 = 0
+        while (currPair < len(lines)):
+            firstSite = lines[currPair][currSite:currSite+1];
+            secondSite = lines[currPair+1][currSite:currSite+1];
             if (firstSite != secondSite):  
                 totalHetero = totalHetero + 1;
             elif (firstSite == "0"):
                 totalHomo0 = totalHomo0 + 1;
             else:
                 totalHomo1 = totalHomo1 + 1;
-            currSite = currSite+1;
-    totalHetero = float(totalHetero);
-    totalHomo0 = float(totalHomo0);
-    totalHomo1 = float(totalHomo1);
-    totalAlleles = float(chromLength * len(lines));
-    p = (2.0*totalHomo0+totalHetero)/totalAlleles;
-    q = 1.0-p;
-    observedHomo0 = float(totalHomo0/(totalAlleles/2));
-    observedHomo1 = float(totalHomo1/(totalAlleles/2));
-    observedHetero = float(totalHetero/(totalAlleles/2));
-    expoHomo0 = p*p;
-    expoHomo1 = q*q;
-    expoHetero = 2*p*q;
-    print("Observed Genotypes: ")
-    print("00: ", observedHomo0)
-    print("Hetero: ", observedHetero)
-    print("11: ", observedHomo1)
-    print("Expected Genotypes: ")
-    print("00: ", expoHomo0)
-    print("Hetero: ", expoHetero)
-    print("11: ", expoHomo1)
-    print("Percentage Difference From Observed and Expected")
-    print("00: ", abs(observedHomo0-expoHomo0)/((expoHomo0+observedHomo0)/2))
-    print("Hetero: ", abs(observedHetero-expoHetero)/((expoHetero+observedHetero)/2))
-    print("11: ", abs(observedHomo1-expoHomo1)/((expoHomo1+observedHomo1)/2))
+            currPair = currPair+2;
+        totalHetero = float(totalHetero);
+        totalHomo0 = float(totalHomo0);
+        totalHomo1 = float(totalHomo1);
+        p = (2.0*totalHomo0+totalHetero)/totalAlleles;
+        q = 1.0-p;
+        observedHomo0 = float(totalHomo0/(totalAlleles/2));
+        observedHomo1 = float(totalHomo1/(totalAlleles/2));
+        observedHetero = float(totalHetero/(totalAlleles/2));
+        expoHomo0 = p*p;
+        expoHomo1 = q*q;
+        expoHetero = 2*p*q;
+        if ((not isInEq(observedHomo0, expoHomo0, boundary)) or (not isInEq(observedHetero, expoHetero, boundary)) or (not isInEq(observedHomo1, expoHomo1, boundary))):
+            isNot.append(currSite)
+        else:
+            isEq.append(currSite)
+        currSite += 1
+    print("Site not in equillibrium:")
+    print(isNot)
+    print("Site in equillibrium: ")
+    print(isEq)
 
+def isInEq(observed, expo, boundary):
+    diff = 0
+    if (observed == 0 and expo == 0):
+        diff = 0
+    elif (observed == 0 and expo > boundary):
+        diff = 1
+    elif (expo == 0 and observed > boundary):
+        diff = 1
+    elif (observed == 0 or expo == 0):
+        diff = 0
+    else:
+        diff = abs(observed-expo)/((expo+observed)/2)
+    if (diff < boundary):
+        return True
+    else:
+        return False
+        
 if __name__ == "__main__":
     main();
