a4e6b5c21cff94f8fa3179afd4d8247f9a754161
Heinrich <maertehp@rose-hulman.edu>
2020-04-12 21:50:59 +0200

    updated code with gene ranges 2

--- Group A/weekend3/Wkend3.java
+++ Group A/weekend3/Wkend3.java
@@ -4,10 +4,7 @@
 package csse490PICalc;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
-
-import csse490PICalc.main.Mutation;
 
 /**
  * @author heinrich
@@ -18,13 +15,13 @@ public class Wkend3 {
 	/**
 	 * @param args
 	 */
-	
+
 	static ArrayList<Mutation> mutations = new ArrayList<Mutation>();
-	
-	static int buffer = 0;
+
+	static int buffer = 0; // changed later on
+
 	public static void main(String[] args) {
-		
-		
+
 		Scanner sc = new Scanner(System.in);
 
 		String current = "";
@@ -45,68 +42,87 @@ public class Wkend3 {
 			mutations.add(m);
 
 		}
-		
+
 		mutations.sort(null);
-		
-		
-		buffer = mutations.get(mutations.size()-1).pos / (mutations.size());
+
+		buffer = mutations.get(mutations.size() - 1).pos / (mutations.size());
 		System.out.println(buffer);
-		
-		
-		for(Mutation m : mutations) {
+
+		for (Mutation m : mutations) {
 			System.out.println(m);
-			
-			
+
 		}
-		
+
 		range();
 
+		sc.close();
 	}
-	
+
 	public static void range() {
-		
+
 		ArrayList<Integer[]> geneRanges = new ArrayList<Integer[]>();
-		
+
 		Integer[] range = new Integer[2];
 		Boolean starting = true;
-		
-		for(int i = 0; i < mutations.size(); i++) {
-			
-			if(mutations.get(i).type.equals("m1")) {
+
+		for (int i = 0; i < mutations.size(); i++) {
+
+			if (mutations.get(i).type.equals("m1")) {
 				continue;
 			}
-			
-			if(starting) {
-				range[0] = mutations.get(i).pos - buffer/2;
+
+			if (starting) {
+				range[0] = mutations.get(i).pos - buffer / 2;
 				starting = false;
 			}
-			
-			if(mutations.get(i+1).type.equals("m1")) {
-				range[1] =  mutations.get(i).pos + buffer/2;
+
+			if (mutations.get(i + 1).type.equals("m1")) {
+				range[1] = mutations.get(i).pos + buffer / 2;
 				geneRanges.add(range);
-				
+
 				range = new Integer[2];
-				
+
 				starting = true;
 			}
 		}
-		
-		
+		// combining genes
+
+		for (int i = geneRanges.size() - 2; i > 0; i--) {
+
+			if (geneRanges.get(i + 1)[0] - geneRanges.get(i)[1] <= 3000) { // <----- unclear what this should be...
+
+				range = new Integer[2];
+
+				range[0] = geneRanges.get(i)[0];
+				range[1] = geneRanges.get(i + 1)[1];
+
+				geneRanges.remove(i + 1);
+				geneRanges.remove(i);
+
+				geneRanges.add(i, range);
+			}
+
+		}
+
 		System.out.println();
 		System.out.println("Predicted gene ranges:");
-		
-		
-		for(Integer[] r : geneRanges) {
-			
-			
-			System.out.println("Starting from "+r[0] +" to "+r[1]);
-			
-			
+
+		@SuppressWarnings("unused")
+		int pre = 0;
+		int counter = 1;
+		for (Integer[] r : geneRanges) {
+
+			System.out.println("Gene " + counter + ": from " + r[0] + " to " + r[1]);
+			counter++;
+
+			// System.out.println("diff: "+(r[0]-pre));
+
+			pre = r[1];
+
 		}
-		
+
 	}
-	
-	
+
 	static class Mutation implements Comparable<Mutation> {
 
 		int id;
@@ -115,7 +131,7 @@ public class Wkend3 {
 
 		String type;
 
-		public Mutation(String input){
+		public Mutation(String input) {
 
 			Scanner sc = new Scanner(input);
 
@@ -130,46 +146,39 @@ public class Wkend3 {
 			sc.close();
 
 		}
-		
-		
-		
+
 		public int compareTo(Mutation m) {
 			return this.pos - m.pos;
 		}
-		
+
 		@Override
 		public boolean equals(Object m) {
-			
-			if(m.getClass().equals(this.getClass())) {
+
+			if (m instanceof Mutation) {
 				Mutation mu = (Mutation) m;
-				if(this.pos == mu.pos && this.type.equals(mu.type)) {
+				if (this.pos == mu.pos && this.type.equals(mu.type)) {
 					return true;
 				}
 			}
-			
+
 			return false;
-			
+
 		}
 
 		@Override
 		public String toString() {
 
 			Boolean gene = true;
-			
-			if(type.equals("m1")) {
+
+			if (type.equals("m1")) {
 				gene = false;
 			}
-			
-			return String.format("Gene: %b, pos (buffered): %2d -> %2d, type = %s, id: %2d", gene, pos-buffer/2,pos+buffer/2, type, id); //id: %2d,  //id
+
+			return String.format("Gene: %b, pos (buffered): %2d -> %2d, type = %s, id: %2d", gene, pos - buffer / 2,
+					pos + buffer / 2, type, id); // id: %2d, //id
 
 		}
-		
-		
-		
-		
 
 	}
-	
-	
 
 }
