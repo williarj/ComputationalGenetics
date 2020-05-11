dcd128c07aa87149d99baa1e44305739ff65ada7
JosephZouRHIT <zous@rose-hulman.edu>
2020-05-11 17:45:06 -0400

    updated tool

--- Group B/Star_Wars/FSTtool.py
+++ Group B/Star_Wars/FSTtool.py
@@ -1,9 +1,10 @@
 import sys
-import matplotlib.pyplot as pyplot
-
+import os
+import matplotlib.pyplot as plt
+import numpy as np
 #usage: folder_name total subpop1 subpop2 ..
-def main:
-	num_file = sys.argc - 2
+def main():
+	num_file = len(sys.argv) - 2
 	raw_h = []
 	for i in range(0, num_file):
 		file = open(sys.argv[2 + i])
@@ -12,20 +13,44 @@ def main:
 	fst = []
 	for i in range(1, len(raw_h)):
 		fst.append(generateFST(raw_h[i], raw_h[0]))
-
+	for i in range(0, num_file - 1):
+		position = []
+		temp_fst = []
+		for j in range(0, len(fst[i])):
+			position.append(fst[i][j][0])
+			temp_fst.append(fst[i][j][1])
+		plt.figure(i)
+		temp_fst_1 = sorted(temp_fst)
+		quartile = np.percentile(temp_fst_1, [25, 75])
+		iqr = quartile[1] - quartile[0]
+		upperbound = quartile[1] + 1.5 * iqr
+		lowerbound = quartile[0] - 1.5 * iqr
+		plt.xticks(np.arange(min(position), 100 * (max(position) / 100 + 1), 50))
+		plt.title('FST vs Sites w/ Outlier Boundaries')
+		plt.xlabel('Site')
+		plt.ylabel('FST')
+		plt.plot(position, np.ones(len(position)) * upperbound, c='r', label='Upperbound')
+		plt.plot(position, np.ones(len(position)) * lowerbound, c='g', label = 'Lowerbound')
+		plt.scatter(position, temp_fst, s=5)
+		plt.legend(bbox_to_anchor=(0.8, 1.15), loc='upper left', borderaxespad=0.)
+		if not os.path.exists(sys.argv[1]):
+			os.makedirs(sys.argv[1])
+		plt.savefig(sys.argv[1] + '/' + sys.argv[3 + i][:len(sys.argv[3 + i]) - 3] + 'png', dpi=300)
 
 
 
 def generateFST(h_s, h_t):
 	fst = []
 	for i in range(0, len(h_s)):
-		fst.append(1-(h_s[i]/h_t[i]))
+		if(h_t[i] == 0):
+			continue
+		fst.append([i, 1-(h_s[i]/h_t[i])])
 	return fst
 
 
 def parsePopulation(lines):
 	h = []
-	for i in range(0, len(lines[3])):
+	for i in range(0, len(lines[3]) - 1):
 		num_zero = 0;
 		for j in range(3, len(lines)):
 			if(int(lines[j][i]) == 0):
