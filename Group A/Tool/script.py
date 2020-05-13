e601701fe4ed21317aeb2693267d735aca4ac952
Keith C-R <keith@kmddigital.com>
2020-05-13 18:10:20 -0400

    Updated tool

--- Group A/Tool/script.py
+++ Group A/Tool/script.py
@@ -2,6 +2,8 @@ import math
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.collections import LineCollection
+from matplotlib.colors import ListedColormap, BoundaryNorm
 
 def main():
     file = open(sys.argv[1], "r")
@@ -27,30 +29,42 @@ def main():
                 maxPos = int(lines[3])
     max = windowSize
     i = 0
-    windowProb = []
+    windowsProb = []
     totalProb = countProbability(0, maxPos, mutations, total)
+    print(totalProb)
     while windowSize*(i+1) <= maxPos:
         posMin = windowSize*i
         posMax = windowSize*(i+1)
-        windowProb.append(countProbability(posMin, posMax, mutations, total)/1)
+        print(countProbability(posMin, posMax, mutations, total))
+        windowsProb.append(countProbability(posMin, posMax, mutations, total)-totalProb)
         i += 1
+    print(windowsProb)
     windows = []
     i = 1
-    while i <= len(windowProb):
-        windows.append(i)
+    while i <= len(windowsProb):
+        windows.append(i*windowSize)
         i = i+1
     fig, ax = plt.subplots()
-    ax.plot(windows, windowProb)
+    windowsProbDiff = np.ma.masked_where(np.abs(windowsProb-np.mean(windowsProb)) <= 1.3*np.std(windowsProb), windowsProb)
+    windowsProb = np.ma.masked_where(np.abs(windowsProb-np.mean(windowsProb)) > 1.3*np.std(windowsProb), windowsProb)
+    plt.scatter(windows, windowsProb, c='g')
+    # scatter warning points in red (c='r')
+    plt.scatter(windows, windowsProbDiff, c='r')
+    ax.plot(windows, windowsProb)
+    ax.set_xlabel('Position')
+    ax.set_ylabel('CLR')
+    ax.grid(True)
     plt.show()
     
 def countProbability(posMin, posMax, mutations, total):
-    count = posMin
+    pos = posMin
     prob = 1.0
-    while count <= posMax:
-        if count in mutations:
-            indProb = mutations[count]/1
-            prob *= (math.factorial(total) / (math.factorial(mutations[count]) * (math.factorial(total-mutations[count])))) * indProb ^ count * (1-indProb) ^ (total-count)
-        count += 1
+    while pos <= posMax:
+        if pos in mutations:
+            indProb = float(mutations[pos])/float(total)
+            probWithoutLog = (math.factorial(total) / (math.factorial(mutations[pos]) * (math.factorial(total-mutations[pos])))) * indProb ** mutations[pos] * (1-indProb) ** (total - mutations[pos])
+            prob += math.log(probWithoutLog)
+        pos += 1
     return prob
 
 if __name__ == "__main__":
